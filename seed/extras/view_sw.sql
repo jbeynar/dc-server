95c39236f518e2e02eb35eb2c924472f85d3e2f5
Jakub Bejnarowicz <jakub.bejnarowicz@gmail.com>
2016-02-23 14:32:36 +0100

    view_sw adjustment

--- seed/extras/view_sw.sql
+++ seed/extras/view_sw.sql
@@ -1,16 +1,16 @@
 DROP VIEW  IF EXISTS sw;
 CREATE VIEW sw AS
 SELECT id,
-document_sw->>'Spolka' as symbol,
-document_sw->>'Sektor' as type,
-(document_sw->>'Kurs zamkniecia')::numeric(9,2) as price,
-document_sw->>'Rating' as rating,
-(document_sw->>'Wartosc ksiegowa')::numeric(9,2) as bv,
-(document_sw->>'Wartosc skorygowanych aktywow netto')::numeric(9,2) as nav_adjusted,
-(document_sw->>'Wyliczona srednia')::numeric(9,2) as value_avg,
+document->>'Spolka' as symbol,
+document->>'Sektor' as type,
+(document->>'Kurs zamkniecia')::numeric(9,2) as price,
+document->>'Rating' as rating,
+(document->>'Wartosc ksiegowa')::numeric(9,2) as bv,
+(document->>'Wartosc skorygowanych aktywow netto')::numeric(9,2) as nav_adjusted,
+(document->>'Wyliczona srednia')::numeric(9,2) as value_avg,
 round((
-	(document_sw->>'Wyliczona srednia')::numeric(9,2)-(document_sw->>'Kurs zamkniecia')::numeric(9,2))/
-			(document_sw->>'Kurs zamkniecia')::numeric(9,2),
+	(document->>'Wyliczona srednia')::numeric(9,2)-(document->>'Kurs zamkniecia')::numeric(9,2))/
+			(document->>'Kurs zamkniecia')::numeric(9,2),
 	2)*100 as potential
-FROM stock
+FROM valuation_sw
 ORDER BY potential DESC
