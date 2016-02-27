45193a0a0ac01d817a8775a2a0b1ccfdc1d241ec
Jakub Bejnarowicz <jakub.bejnarowicz@gmail.com>
2016-02-27 18:19:11 +0100

    new extraction map format + synchro process fn

--- libs/extraction.js
+++ libs/extraction.js
@@ -1,23 +1,48 @@
 'use strict';
 
-var promise = require('bluebird');
-var cheerio = require('cheerio');
-var _ = require('lodash');
+const promise = require('bluebird');
+const cheerio = require('cheerio');
+const _ = require('lodash');
+
+const errorCode = {
+    docEmpty: 'ERR_DOC_EMPTY',
+    mapInvalid: 'ERR_MAP_INVALID'
+};
 
 function extract(doc, map)
 {
-    return new promise((resolve) =>
+    return new Promise((resolve, reject) =>
     {
-        var $ = cheerio.load(doc);
-        var extracted = {};
-        _.forEach(map, function (mapItem)
+        var $, extracted = {};
+
+        function extractOnce(def, key)
         {
-            extracted[mapItem.as] = $(mapItem.select).text();
-        });
+            var selector = _.isString(def) ? def : def.selector;
+
+            var match = $(selector).text();
+            if (_.isObject(def) && def.process && def.process instanceof Function) {
+                match = def.process('' + match);
+            }
+            extracted[key] = match;
+        }
+
+        if (_.isEmpty(doc)) {
+            return reject(new Error(errorCode.docEmpty));
+        }
+
+        if (!_.isObject(map)) {
+            return reject(new Error(errorCode.mapInvalid));
+        }
+
+        $ = cheerio.load(doc);
+
+        _.forEach(map, extractOnce);
+
         return resolve(extracted);
     });
 }
 
 module.exports = {
-    extract: extract
+    extract: extract,
+    errorCodes: errorCode
 };
