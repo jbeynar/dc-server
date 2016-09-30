0a27880627fee7578d32901521f4fa4055a71199
jbeynar <jakub.bejnarowicz@gmail.com>
2016-09-30 16:47:27 +0200

    mongodb export on rxjs

--- libs/repo/ExportMongo.js
+++ libs/repo/ExportMongo.js
@@ -1,25 +1,99 @@
-const rfr = require('rfr');
-const DocumentDAO = rfr('libs/repo/DocumentDAO');
-const _ = require('lodash');
-var mongo = require('mongodb').MongoClient;
-var mongodbUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017/stocks';
-const promise = require('bluebird');
+'use strict';
 
-function exportIntoMongo(connectionUrl, type) {
-    return DocumentDAO.getJsonDocuments({type: type}).then(function (res) {
-        return res.results
-    }).then(function (stocks) {
-        return new promise(function (resolve, reject) {
-            mongo.connect(connectionUrl, function (err, client) {
-                var stocksCollection = client.collection('stock');
-                stocksCollection.insertMany(stocks, function () {
-                    console.log(arguments);
-                    resolve();
+const _ = require('lodash');
+const Promise = require('bluebird');
+const mongo = require('mongodb').MongoClient;
+const pg = require('pg-rxjs');
+const squel = require('squel').useFlavour('postgres');
+const rfr = require('rfr');
+
+const config = rfr('config');
+
+var exportConfig = {
+    sourcePostgresqlUrl: config.db.connectionUrl,
+    sourceTypeName: 'product',
+    targetMongoDbUrl: 'mongodb://localhost:27017/food-scanner',
+    targetCollectionName: 'ingredient'
+};
+
+function handleError(err) {
+    if (err) {
+        console.log('MongoDb Exception');
+        throw err;
+    }
+}
+
+function dropCollections(collectionNames) {
+    return new Promise((resolve,reject)=>{
+        mongo.connect(exportConfig.targetMongoDbUrl, function (err, client) {
+            return Promise.map(collectionNames, function (collectionName) {
+                return new Promise(function (resolve, reject) {
+                    client.collection(collectionName).drop(function (err) {
+                        if (err && err.message !== 'ns not found') {
+                            reject(err);
+                        } else {
+                            resolve();
+                        }
+                    });
                 });
-                console.log('Inserted');
+            }).catch((err)=>{
+                console.log('Can not remove some collection');
+                throw err;
+                reject();
+            }).finally(()=>{
+                console.log('Collection removed');
+                resolve();
             });
         });
     });
-};
+}
 
-return exportIntoMongo(mongodbUrl, 'valuation');
+function exportIntoMongo(exportConfig) {
+    return new Promise((resolve)=> {
+        mongo.connect(exportConfig.targetMongoDbUrl, function (err, client) {
+            handleError(err);
+            const pool = pg.Pool(exportConfig.sourcePostgresqlUrl, { debug: false });
+            const query = squel.select()
+                .from('repo.document_json')
+                .field('id', 'source_id')
+                .field('body')
+                .where('type=?', exportConfig.sourceTypeName)
+                .toString();
+
+            console.log('Streaming source repo documents', exportConfig.sourceTypeName, 'from',
+                exportConfig.sourcePostgresqlUrl, 'into', exportConfig.targetMongoDbUrl, 'as',
+                exportConfig.sourceTypeName);
+
+            pool.stream(query)
+                .bufferWithCount(100)
+                .flatMap((documentsSet) => {
+                    return new Promise((resolve, reject)=> {
+                        var targetCollection = client.collection(exportConfig.targetCollectionName, {}, function (err) {
+                            handleError(err);
+                        });
+
+                        targetCollection.insertMany(_.map(documentsSet, (sourceDoc)=> {
+                                sourceDoc.body._source_id = sourceDoc._id;
+                                return sourceDoc.body;
+                            }),
+                            {},
+                            function (err) {
+                                handleError(err);
+                                process.stdout.write('.');
+                                resolve();
+                            });
+                    });
+                })
+                .subscribe(()=> {}, () => {},
+                    (data) => {
+                        console.log('\nSuccess');
+                        return resolve();
+                    }
+                );
+        });
+    });
+}
+
+dropCollections([exportConfig.targetCollectionName]).then(()=> {exportIntoMongo(exportConfig)});
+
+
