f7bfc7f3975142ea6e88d909ad874ad263d1336f
jbeynar <jakub.bejnarowicz@gmail.com>
2016-10-10 11:47:40 +0200

    downloader tests and urls function

--- libs/downloader.js
+++ libs/downloader.js
@@ -2,72 +2,70 @@
 
 const _ = require('lodash');
 const db = require('./db');
-const DocumentDAO = require('./repo');
-const promise = require('bluebird');
+const repo = require('./repo');
+const Promise = require('bluebird');
 const urlInfoService = require('url');
 
 const Curl = require('node-libcurl').Curl;
 
 const defaultOptions = {
-    downloadDelay: 500,
-    silent: false
+    downloadDelay: 500
 };
 
-function downloadHttpDocuments(urls, httpHeaders)
-{
-    if (!_.isArray(urls)) {
-        throw new Error('urls param must be an array');
+function downloadHttpDocuments(downloadJob) {
+
+    if (_.isFunction((downloadJob.urls))) {
+        downloadJob.urls = downloadJob.urls();
+    } else if (!_.isArray(downloadJob.urls)) {
+        throw new Error('downloadJob.urls param must be an array');
     }
 
     var options = defaultOptions;
 
-    return db.connect().then(function (client)
-    {
-        var i = 0;
-        return promise.each(urls, function (url)
-        {
-            process.stdout.write(++i + '/' + urls.length + ' ' + url);
-            return new promise(function (resolve)
-            {
-                var curl = new Curl();
-                curl.setOpt(Curl.option.URL, url);
-                curl.setOpt(Curl.option.FOLLOWLOCATION, 1);
-                curl.setOpt(Curl.option.TIMEOUT, 30);
+    return Promise.resolve(downloadJob.urls).then((urls)=> {
+        return db.connect().then(function (client) {
+            var i = 0;
+            return Promise.each(urls, function (url) {
+                process.stdout.write(++i + '/' + urls.length + ' ' + url);
+                return new Promise(function (resolve) {
+                    var curl = new Curl();
+                    curl.setOpt(Curl.option.URL, url);
+                    curl.setOpt(Curl.option.FOLLOWLOCATION, 1);
+                    curl.setOpt(Curl.option.TIMEOUT, 30);
 
-                if (!_.isEmpty(httpHeaders)) {
-                    curl.setOpt(Curl.option.HTTPHEADER, httpHeaders);
-                }
+                    if (_.get(downloadJob, 'options.headers') && !_.isEmpty(downloadJob.options.headers)) {
+                        curl.setOpt(Curl.option.HTTPHEADER, downloadJob.options.headers);
+                    }
 
-                curl.on('end', function (statusCode, body, headers)
-                {
-                    let urlInfo = urlInfoService.parse(url);
-                    let data = {
-                        type: curl.getInfo(Curl.info.CONTENT_TYPE),
-                        url: url,
-                        host: urlInfo.hostname,
-                        path: urlInfo.pathname,
-                        query: urlInfo.query,
-                        code: statusCode,
-                        headers: JSON.stringify(headers),
-                        body: body,
-                        length: body.length
-                    };
-                    process.stdout.write(' [' + data.code + ']\n');
-                    DocumentDAO.saveHttpDocument(data).then(resolve);
-                    this.close();
-                });
+                    curl.on('end', function (statusCode, body, headers) {
+                        let urlInfo = urlInfoService.parse(url);
+                        let data = {
+                            type: curl.getInfo(Curl.info.CONTENT_TYPE),
+                            url: url,
+                            host: urlInfo.hostname,
+                            path: urlInfo.pathname,
+                            query: urlInfo.query,
+                            code: statusCode,
+                            headers: JSON.stringify(headers),
+                            body: body,
+                            length: body.length
+                        };
+                        process.stdout.write(' [' + data.code + ']\n');
+                        repo.saveHttpDocument(data).then(resolve);
+                        this.close();
+                    });
 
-                curl.on('error', function (error)
-                {
-                    console.log('Downloader error');
-                    console.log(error);
-                });
+                    curl.on('error', function (error) {
+                        console.log('Downloader error');
+                        console.log(error);
+                    });
 
-                curl.perform();
+                    curl.perform();
 
-            }).delay(options.downloadDelay);
-        }).finally(client.done);
-    }).catch(db.exceptionHandler);
+                }).delay(options.downloadDelay);
+            }).finally(client.done);
+        }).catch(db.exceptionHandler);
+    });
 }
 
 module.exports = {
---
+++ test/spec/downloader.js
@@ -0,0 +1,119 @@
+'use strict';
+
+const _ = require('lodash');
+const Promise = require('bluebird');
+const expect = require('chai').expect;
+const proxyquire = require('proxyquire');
+
+describe('Downloader library', () => {
+    var downloader;
+    var mockRepoSavedHttpDocuments = [];
+
+    before(()=> {
+        const repoLibMock = {
+            saveHttpDocument: (data)=> {
+                mockRepoSavedHttpDocuments.push(data);
+                return Promise.resolve();
+            }
+        };
+
+        downloader = proxyquire('../../libs/downloader', {
+            './repo': repoLibMock
+        });
+    });
+
+    afterEach(()=> {
+        mockRepoSavedHttpDocuments = [];
+    });
+
+    describe('downloads documents', ()=> {
+        it('based on urls array', function () {
+            const downloadJob = {
+                urls: ['https://code.jquery.com/jquery-3.1.1.js',
+                    'https://code.jquery.com/jquery-2.2.4.js']
+            };
+            const expectedHttpDocuments = [
+                {
+                    type: 'application/javascript; charset=utf-8',
+                    url: 'https://code.jquery.com/jquery-3.1.1.js',
+                    host: 'code.jquery.com',
+                    path: '/jquery-3.1.1.js',
+                    query: null,
+                    code: 200,
+                    length: 267194
+                },
+                {
+                    type: 'application/javascript; charset=utf-8',
+                    url: 'https://code.jquery.com/jquery-2.2.4.js',
+                    host: 'code.jquery.com',
+                    path: '/jquery-2.2.4.js',
+                    query: null,
+                    code: 200,
+                    length: 257551
+                }
+            ];
+            return downloader.downloadHttpDocuments(downloadJob).then(()=> {
+                expect(_.omit(mockRepoSavedHttpDocuments[0], 'body', 'headers')).to.eql(expectedHttpDocuments[0]);
+                expect(_.omit(mockRepoSavedHttpDocuments[1], 'body', 'headers')).to.eql(expectedHttpDocuments[1]);
+            });
+        });
+
+        it('based on urls synchronous function', ()=> {
+            const downloadJob = {
+                urls: function () {
+                    return _(_.range(1, 3)).map((i)=> { return `http://www.tarnowiak.pl/ogloszenie/${i}/a/`;}).value();
+                }
+            };
+            const expectedHttpDocuments = [
+                {
+                    'code': 200,
+                    'host': 'www.tarnowiak.pl',
+                    'path': '/ogloszenie/1/a/',
+                    'url': 'http://www.tarnowiak.pl/ogloszenie/1/a/'
+                },
+                {
+                    'code': 200,
+                    'host': 'www.tarnowiak.pl',
+                    'path': '/ogloszenie/2/a/',
+                    'url': 'http://www.tarnowiak.pl/ogloszenie/2/a/'
+                }
+            ];
+            return downloader.downloadHttpDocuments(downloadJob).then(()=> {
+                const whitelist = ['code', 'host', 'path', 'url'];
+                expect(_.pick(mockRepoSavedHttpDocuments[0], whitelist)).to.eql(expectedHttpDocuments[0]);
+                expect(_.pick(mockRepoSavedHttpDocuments[1], whitelist)).to.eql(expectedHttpDocuments[1]);
+            });
+        });
+        it('based on urls asynchronous function', ()=> {
+            const downloadJob = {
+                urls: function () {
+                    return new Promise((resolve)=> {
+                        setTimeout(()=> {
+                            resolve(_(_.range(1, 3)).map(
+                                (i)=> { return `http://www.tarnowiak.pl/ogloszenie/${i}/a/`;}).value());
+                        }, 500);
+                    });
+                }
+            };
+            const expectedHttpDocuments = [
+                {
+                    'code': 200,
+                    'host': 'www.tarnowiak.pl',
+                    'path': '/ogloszenie/1/a/',
+                    'url': 'http://www.tarnowiak.pl/ogloszenie/1/a/'
+                },
+                {
+                    'code': 200,
+                    'host': 'www.tarnowiak.pl',
+                    'path': '/ogloszenie/2/a/',
+                    'url': 'http://www.tarnowiak.pl/ogloszenie/2/a/'
+                }
+            ];
+            return downloader.downloadHttpDocuments(downloadJob).then(()=> {
+                const whitelist = ['code', 'host', 'path', 'url'];
+                expect(_.pick(mockRepoSavedHttpDocuments[0], whitelist)).to.eql(expectedHttpDocuments[0]);
+                expect(_.pick(mockRepoSavedHttpDocuments[1], whitelist)).to.eql(expectedHttpDocuments[1]);
+            });
+        });
+    });
+});
