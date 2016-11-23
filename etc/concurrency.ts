baa70d1a99c2980093e4d6d47407b9faf702bde1
jbeynar <jakub.bejnarowicz@gmail.com>
2016-11-23 20:56:06 +0100

    concurrency rxjs processing and socket.io notification

---
+++ etc/concurrency.ts
@@ -0,0 +1,73 @@
+'use strict';
+
+import * as Rx from 'rxjs';
+import * as _ from 'lodash';
+
+var io = require('socket.io')(3333);
+
+io.on('connection', function (socket) {
+
+    const source = Rx.Observable.from([
+        {
+            id: '8',
+            size: [9, 2]
+        },
+        {
+            id: '9',
+            size: [8, 5]
+        },
+        {
+            id: 'A',
+            size: [2, 1]
+        },
+        {
+            id: 'B',
+            size: [1, 6]
+        },
+        {
+            id: 'C',
+            size: [3, 4]
+        },
+        {
+            id: 'D',
+            size: [9, 3]
+        },
+        {
+            id: 'E',
+            size: [9, 1]
+        },
+        {
+            id: 'F',
+            size: [9, 3]
+        }
+    ]);
+
+    function broadcast(task, number) {
+        task.progress = number;
+        console.log(task.id, number);
+        io.emit('public', task);
+    }
+
+    source.mergeMap((x: any): Promise<number> => {
+        broadcast(x, 10);
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                broadcast(x, 30);
+                return resolve(x);
+            }, 500 * x.size[0]);
+        });
+    }, 2).concat().mergeMap((x: any) => {
+        broadcast(x, 50);
+        let promise = new Promise((resolve) => {
+            setTimeout(() => {
+                broadcast(x, 70);
+                resolve(x);
+            }, 1000 * x.size[1]);
+        });
+        return Rx.Observable.fromPromise(promise);
+    }, 1).subscribe((data: any) => {
+        broadcast(data, 100);
+    });
+});
+
+
