72efc5675ef671c147568712ae907fb8037c66f0
jbeynar <jakub.bejnarowicz@gmail.com>
2016-12-21 20:39:21 +0100

    db search path WIP

--- libs/db.ts
+++ libs/db.ts
@@ -3,12 +3,23 @@
 import * as pg from 'pg';
 import * as Promise from 'bluebird';
 import {config} from '../config';
+import {Client} from "../node_modules/@types/pg/index";
 
 var highlightStart = '\x1b[31m';
 var highlightEnd = '\x1b[0m';
 var pool = new pg.Pool(config.db.poolConfig);
 
-query(`SET search_path TO ${config.db.schema}`);
+let searchPathSet = false;
+
+function setSearchPath(client: Client): Promise<Client> {
+    if (searchPathSet) {
+        return Promise.resolve(client);
+    } else {
+        // todo why it doesn't work?
+        // searchPathSet = true;
+        return Promise.resolve(client.query(`SET search_path TO ${config.db.schema}`).then(() => client));
+    }
+}
 
 export function exceptionHandler(err) {
     console.error(highlightStart + 'SQL ' + err.toString());
@@ -21,17 +32,15 @@ export function exceptionHandler(err) {
     console.log(err.stack, highlightEnd);
 }
 
-export function query(query: string, bindings?: string[]|number[]): Promise<any> {
+export function query(query: string, bindings?: string[]|number[]): Promise<any[]> {
     return new Promise((resolve) => {
-        return pool.connect().then(function (client) {
-            client.query(`SET search_path TO ${config.db.schema}`).then(() => {
-                client.query(query, bindings).then(function (res) {
-                    client.release();
-                    resolve(res.rows);
-                }).catch(exceptionHandler);
+        pool.connect().then(setSearchPath).then((client) => {
+            client.query(query, bindings).then((res) => {
+                client.release();
+                resolve(res.rows);
             }).catch(exceptionHandler);
         }).catch(exceptionHandler);
-    }).catch(exceptionHandler);
+    }).catch(exceptionHandler) as Promise<any[]>;
 }
 
 export function getPool(){
