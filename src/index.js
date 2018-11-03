b9de8b775a21c0a79fe0cbaa09f26fceec9cc6d9
jbeynar <jakub.bejnarowicz@gmail.com>
2018-11-03 08:13:07 +0100

    move src into separated repository

--- src/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const jobLib = require('job');
-const exampleJob = require('fake-site.job');
-
-jobLib.executeJob(exampleJob).subscribe(console.log);
--- src/operators/extract.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function decorateWithOperator(step, observable) {
-    return observable;
-}
-
-module.exports = {
-    decorateWithOperator
-};
